caa6b43cb2f0a5e945ca369692bcc3af3a1e9635
Francisco-Neto13 <franciscoaneto13@gmail.com>
2026-03-14 21:45:30 -0300

    fix(deploy): fix house image asset path

--- src/components/House/House.jsx
+++ src/components/House/House.jsx
@@ -1,10 +1,10 @@
 const House = () => {
   return (
     <div className="absolute top-[304px] left-0 w-[390px] h-[390px] flex items-center justify-center pointer-events-none">
-      <img 
-        src="../../../public/images/house.png" 
-        alt="House" 
-        className="w-[390px] h-[390px] object-contain" 
+      <img
+        src="/images/house.png"
+        alt="House"
+        className="w-[390px] h-[390px] object-contain"
       />
     </div>
   );
